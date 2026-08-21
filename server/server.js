8b6f315a5044ad7c0670cddf9297c2c364851c7c
Varun Kumar <varunkumarvelpula949@gmail.com>
2026-08-21 23:18:12 +0530

    Fix CORS

--- server/server.js
+++ server/server.js
@@ -8,26 +8,31 @@ const taskRoutes = require("./routes/taskRoutes");
 
 const app = express();
 
+// CORS
 app.use(
   cors({
     origin: [
       "http://localhost:3000",
-      "https://task-management-app-chi-gold.vercel.app",
+      "https://task-management-app-tsap-4yzfzib71.vercel.app",
     ],
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
 
+// JSON
 app.use(express.json());
 
+// Test backend
 app.get("/", (req, res) => {
   res.send("Task Management API is running!");
 });
 
+// Routes
 app.use("/api/auth", authRoutes);
 app.use("/api/tasks", taskRoutes);
 
+// MongoDB
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => {
@@ -37,6 +42,7 @@ mongoose
     console.log("MongoDB connection failed:", error.message);
   });
 
+// Local server
 if (process.env.NODE_ENV !== "production") {
   app.listen(5000, () => {
     console.log("Server running on port 5000");
